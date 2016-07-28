0a16bb156805c1b52bff3b76e878c0da69def30a
hahwul <hahwul@gmail.com>
2016-07-28 11:28:11 +0900

    1.3.8

---
+++ install.sh
@@ -0,0 +1,13 @@
+#/usr/bin/bash
+echo 'Install a2sv'
+echo ' -> install python package'
+echo ' -> pip:argparse'
+pip install argparse
+echo ' -> pip:netaddr'
+pip install netaddr
+echo ' -> install openssl(apt)'
+apt-get install openssl
+echo ' -> set command'
+MYPWD=`pwd`
+echo '#/usr/bin/python\npython '$MYPWD'a2sv.py' >> /usr/bin/a2sv
+echo 'Finish. run a a2sv'
