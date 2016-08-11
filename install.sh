8a24ba818b8dcf966c65171a0548695b9da83145
hahwul <hahwul@gmail.com>
2016-08-12 01:13:30 +0900

    v1.4

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ echo ' -> install openssl(apt)'
 apt-get install openssl
 echo ' -> set command'
 MYPWD=`pwd`
-echo '#/usr/bin/python\npython '$MYPWD'a2sv.py' >> /usr/bin/a2sv
+echo '#/usr/bin/python
+python '$MYPWD'/a2sv.py' >> /usr/bin/a2sv
 echo 'Finish. run a a2sv'
