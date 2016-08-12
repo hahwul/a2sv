fcc32e2fc03d24a11e810b666cd0ba237440c2dc
hahwul <hahwul@gmail.com>
2016-08-12 17:56:32 +0900

    v1.4

--- install.sh
+++ install.sh
@@ -10,5 +10,7 @@ apt-get install openssl
 echo ' -> set command'
 MYPWD=`pwd`
 echo '#/usr/bin/python
-python '$MYPWD'/a2sv.py' >> /usr/bin/a2sv
+python '$MYPWD'/a2sv.py $*' >> /usr/bin/a2sv
+echo 'Set Perm'
+chmod 755 /usr/bin/a2sv
 echo 'Finish. run a a2sv'
